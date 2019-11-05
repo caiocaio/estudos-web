872d58b8d5aa978f53c9402cd4fc6de0f5686058
caiocaio <caiomchaves@gmail.com>
2019-11-05 20:58:20 -0300

    ter5/11/19-2

---
+++ estruturas de controle/dowhile.js
@@ -0,0 +1,18 @@
+function getInteiroAleatorioEntre(min, max){
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = -1
+
+do {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    //console.log(`opcao escolhida foi ${opcao}.`)
+    console.log('Opcao escolhida foi ' + opcao)
+}while(opcao != -1)
+
+console.log('Ate a proxima!')
+
+
+
+//pouco usada!!!!!!!!!!!! 
