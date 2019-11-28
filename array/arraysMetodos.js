380510f71f680719d0007836c2473306ff44dca5
caiocaio <caiomchaves@gmail.com>
2019-11-27 22:37:08 -0300

    qua27/11/19

---
+++ array/arraysMetodos.js
@@ -0,0 +1,29 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa ']
+pilotos.pop() // remove o ultimo elemto do array
+console.log(pilotos)
+
+pilotos.push('Verstappen')
+console.log(pilotos)
+
+pilotos.shift()   //tira o primeiro elemento do array
+console.log(pilotos)
+
+pilotos.unshift('Hamilton')  //coloca um elemento no primiero indice[0] do array 
+console.log(pilotos)
+
+//splice pode adicionar e remover elementos do array
+
+
+//adicionar
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+//remover
+pilotos.splice(3, 1) 
+console.log(pilotos)
+
+const algunsPilotos1 = pilotos.slice(2) //vai fazer um novo array apartir do indice indicado
+console.log(algunsPilotos1)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
