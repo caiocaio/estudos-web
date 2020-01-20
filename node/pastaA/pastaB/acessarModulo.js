8da35bee1b10431f40fafa6076631baf659e6df3
caiocaio <caiomchaves@gmail.com>
2020-01-20 11:06:31 -0300

    seg20/01/2020

--- node/pastaA/pastaB/acessarModulo.js
+++ node/pastaA/pastaB/acessarModulo.js
@@ -1,2 +1,14 @@
 const moduloA = require('../../moduloA') //prestar atenção na forma que está escrito, no linux deve-se respeitar o caseSensitive
 console.log(moduloA.ola) 
+
+const saudacao = require('saudacao')  // ele procura o arquivo index.js
+console.log(saudacao.ola)
+
+const c = require('./pastaC/index')
+console.log(c.ola2)
+
+const http = require('http')
+http.createServer((req, res) => {
+    res.write('Bom dia!')
+    res.end()
+}).listen(8080)
